257922a7b4824a685eec667dd0fe433be080ba46
Noam Ringach <noamringach@dhcp-vl2041-28868.eduroam.cornell.edu>
2023-10-03 17:05:39 -0400

    Added a bunch of try catch to get signing exceptions... should remove later

--- phase1/src/phase1/server/implementation/Phase1ServerImpl.java
+++ phase1/src/phase1/server/implementation/Phase1ServerImpl.java
@@ -13,10 +13,8 @@ import types.responses.implementation.AuthenticatedRegisterResponse;
 
 import java.io.IOException;
 import java.io.Serializable;
-import java.security.KeyFactory;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.Signature;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Hashtable;
@@ -30,108 +28,116 @@ public class Phase1ServerImpl<K extends Serializable, V extends Serializable, M 
 
   private Phase0Impl<K, V, M> store;
 
-  public Phase1ServerImpl() throws IOException { 
-    super(); 
+  public Phase1ServerImpl() throws IOException {
+    super();
     this.signingKey = super.signingKey;
     this.verificationKey = super.verificationKey;
     this.activeUsers = new Hashtable<>();
     this.store = new Phase0Impl<>();
   }
 
-  private PrivateKey createPrivateKey(byte[] privateBytes) {
+  private PrivateKey createPrivateKey(byte[] privateBytes) throws NoSuchAlgorithmException, InvalidKeySpecException {
     return KeyFactory.getInstance("DSA").generatePrivate(new PKCS8EncodedKeySpec(privateBytes));
   }
 
-  private PublicKey createPublicKey(byte[] publicBytes) {
+  private PublicKey createPublicKey(byte[] publicBytes) throws NoSuchAlgorithmException, InvalidKeySpecException {
     return KeyFactory.getInstance("DSA").generatePublic(new X509EncodedKeySpec(publicBytes));
   }
 
   public AbstractAuthenticatedRegisterResponse authenticatedRegister(AbstractAuthenticatedRegisterRequest req) {
-    Signature sign = Signature.getInstance("DSA");
-    sign.initVerify(this.createPublicKey(req.verificationKey));
-    sign.update(req.userId.getBytes()); // We shouldn't just be signing the userID – V. vulnerable to replay attacks
-    boolean valid = sign.verify(req.digitalSignature);
+    try {
+      Signature sign = Signature.getInstance("DSA");
+      sign.initVerify(this.createPublicKey(req.verificationKey));
+      sign.update(req.userId.getBytes()); // We shouldn't just be signing the userID – V. vulnerable to replay attacks
+      boolean valid = sign.verify(req.digitalSignature);
 
-    AbstractAuthenticatedRegisterResponse.Status status;
-    if (!valid) {
-      status = AbstractAuthenticatedRegisterResponse.Status.AuthenticationFailure;
-    } else if (activeUsers.containsKey(req.userId)) {
-      status = AbstractAuthenticatedRegisterResponse.Status.UserAlreadyExists;
+      AbstractAuthenticatedRegisterResponse.Status status;
+      if (!valid) {
+        status = AbstractAuthenticatedRegisterResponse.Status.AuthenticationFailure;
+      } else if (activeUsers.containsKey(req.userId)) {
+        status = AbstractAuthenticatedRegisterResponse.Status.UserAlreadyExists;
+      } else {
+        activeUsers.put(req.userId, req.verificationKey);
+        status = AbstractAuthenticatedRegisterResponse.Status.OK;
+      }
+      Signature server_sign = Signature.getInstance("DSA");
+      server_sign.initSign(this.createPrivateKey(this.signingKey));
+      server_sign.update(status.name().getBytes());
+
+
+      return new AuthenticatedRegisterResponse(status, server_sign.sign());
+    } catch (Exception e) {
+      return null;
     }
-    else {
-      activeUsers.put(req.userId, req.verificationKey);
-      status = AbstractAuthenticatedRegisterResponse.Status.OK;
-    }
-    Signature server_sign = Signature.getInstance("DSA");
-    server_sign.initSign(this.createPrivateKey(this.signingKey));
-    server_sign.update(status.name().getBytes());
-
-
-    return new AuthenticatedRegisterResponse(status, server_sign.sign());
   }
 
   public AbstractAuthenticatedDoResponse<K, V, M> authenticatedDo(AbstractAuthenticatedDoRequest<K, V, M> req) {
-    Signature sign = Signature.getInstance("DSA");
-    sign.initVerify(this.createPublicKey(this.activeUsers.get(req.userId)));
-    sign.update(req.userId.getBytes()); // Figure out what to actually have signed here (userId + operation???)
-    boolean valid = sign.verify(req.digitalSignature);
+    try {
+      Signature sign = Signature.getInstance("DSA");
+      sign.initVerify(this.createPublicKey(this.activeUsers.get(req.userId)));
+      sign.update(req.userId.getBytes()); // Figure out what to actually have signed here (userId + operation???)
+      boolean valid = sign.verify(req.digitalSignature);
 
-    DoOperationOutcome.Outcome outcome = DoOperationOutcome.Outcome.AUTHENTICATION_FAILURE;
-    K key = req.doOperation.key;
-    V val = req.doOperation.val;
-    M metaVal = req.doOperation.metaVal;;
-    if (!valid) {
-      outcome = DoOperationOutcome.Outcome.AUTHENTICATION_FAILURE;
-    } else {
-      switch (req.doOperation.operation) {
-        case CREATE:
-          store.create(req.doOperation.key, req.doOperation.val, req.doOperation.metaVal);
-          outcome = DoOperationOutcome.Outcome.SUCCESS;
-          break;
-        case DELETE:
-          store.delete(req.doOperation.key);
-          outcome = DoOperationOutcome.Outcome.SUCCESS;
-          break;
-        case READVAL:
-          try {
-            val = store.readVal(req.doOperation.key);
+      DoOperationOutcome.Outcome outcome = DoOperationOutcome.Outcome.AUTHENTICATION_FAILURE;
+      K key = req.doOperation.key;
+      V val = req.doOperation.val;
+      M metaVal = req.doOperation.metaVal;
+      ;
+      if (!valid) {
+        outcome = DoOperationOutcome.Outcome.AUTHENTICATION_FAILURE;
+      } else {
+        switch (req.doOperation.operation) {
+          case CREATE:
+            store.create(req.doOperation.key, req.doOperation.val, req.doOperation.metaVal);
             outcome = DoOperationOutcome.Outcome.SUCCESS;
-          } catch (NoSuchElementException e) {
-            outcome = DoOperationOutcome.Outcome.NOSUCHELEMENT;
-          }
-          break;
-        case WRITEVAL:
-          try {
-            store.writeVal(req.doOperation.key, req.doOperation.val);
+            break;
+          case DELETE:
+            store.delete(req.doOperation.key);
             outcome = DoOperationOutcome.Outcome.SUCCESS;
-          } catch (IllegalArgumentException e) {
-            outcome = DoOperationOutcome.Outcome.ILLEGALARGUMENT;
-          }
-          break;
-        case READMETAVAL:
-          try {
-            metaVal = store.readMetaVal(req.doOperation.key);
-            outcome = DoOperationOutcome.Outcome.SUCCESS;
-          } catch (NoSuchElementException e) {
-            outcome = DoOperationOutcome.Outcome.NOSUCHELEMENT;
-          }
-          break;
-        case WRITEMETAVAL:
-          try {
-            store.writeMetaVal(req.doOperation.key, req.doOperation.metaVal);
-            outcome = DoOperationOutcome.Outcome.SUCCESS;
-          } catch (IllegalArgumentException e) {
-            outcome = DoOperationOutcome.Outcome.ILLEGALARGUMENT;
-          }
-          break;
+            break;
+          case READVAL:
+            try {
+              val = store.readVal(req.doOperation.key);
+              outcome = DoOperationOutcome.Outcome.SUCCESS;
+            } catch (NoSuchElementException e) {
+              outcome = DoOperationOutcome.Outcome.NOSUCHELEMENT;
+            }
+            break;
+          case WRITEVAL:
+            try {
+              store.writeVal(req.doOperation.key, req.doOperation.val);
+              outcome = DoOperationOutcome.Outcome.SUCCESS;
+            } catch (IllegalArgumentException e) {
+              outcome = DoOperationOutcome.Outcome.ILLEGALARGUMENT;
+            }
+            break;
+          case READMETAVAL:
+            try {
+              metaVal = store.readMetaVal(req.doOperation.key);
+              outcome = DoOperationOutcome.Outcome.SUCCESS;
+            } catch (NoSuchElementException e) {
+              outcome = DoOperationOutcome.Outcome.NOSUCHELEMENT;
+            }
+            break;
+          case WRITEMETAVAL:
+            try {
+              store.writeMetaVal(req.doOperation.key, req.doOperation.metaVal);
+              outcome = DoOperationOutcome.Outcome.SUCCESS;
+            } catch (IllegalArgumentException e) {
+              outcome = DoOperationOutcome.Outcome.ILLEGALARGUMENT;
+            }
+            break;
+        }
       }
+      DoOperationOutcome<K, V, M> doOperationOutcome = new DoOperationOutcome<>(key, val, metaVal, outcome);
+
+      Signature server_sign = Signature.getInstance("DSA");
+      server_sign.initSign(this.createPrivateKey(this.signingKey));
+      server_sign.update(outcome.name().getBytes());
+
+      return new AuthenticatedDoResponse<K, V, M>(doOperationOutcome, server_sign.sign());
+    } catch (Exception e) {
+      return null;
     }
-    DoOperationOutcome<K,V,M> doOperationOutcome = new DoOperationOutcome<>(key, val, metaVal, outcome);
-
-    Signature server_sign = Signature.getInstance("DSA");
-    server_sign.initSign(this.createPrivateKey(this.signingKey));
-    server_sign.update(outcome.name().getBytes());
-
-    return new AuthenticatedDoResponse<K, V, M>(doOperationOutcome, server_sign.sign());
   }
 }
--- phase1/src/phase1/stub/implementation/Phase1StubImpl.java
+++ phase1/src/phase1/stub/implementation/Phase1StubImpl.java
@@ -15,6 +15,7 @@ import java.io.Serializable;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.security.*;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Hashtable;
@@ -25,7 +26,7 @@ public class Phase1StubImpl<K extends Serializable, V extends Serializable, M ex
   private final byte[] serverVerificationKey;
 
   private final Hashtable<String, KeyPair> userKeys;
-  
+
   public Phase1StubImpl() throws IOException, NotBoundException {
     super();
     this.serverVerificationKey = super.serverVerificationKey;
@@ -33,155 +34,184 @@ public class Phase1StubImpl<K extends Serializable, V extends Serializable, M ex
     this.userKeys = new Hashtable<>();
   }
 
-  private PrivateKey createPrivateKey(byte[] privateBytes) {
+  private PrivateKey createPrivateKey(byte[] privateBytes) throws NoSuchAlgorithmException, InvalidKeySpecException {
     return KeyFactory.getInstance("DSA").generatePrivate(new PKCS8EncodedKeySpec(privateBytes));
   }
 
-  private PublicKey createPublicKey(byte[] publicBytes) {
+  private PublicKey createPublicKey(byte[] publicBytes) throws NoSuchAlgorithmException, InvalidKeySpecException {
     return KeyFactory.getInstance("DSA").generatePublic(new X509EncodedKeySpec(publicBytes));
   }
 
-  private byte[] createSignature(PrivateKey privateKey, byte[] contents) {
+  private byte[] createSignature(PrivateKey privateKey, byte[] contents) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
     Signature server_sign = Signature.getInstance("DSA");
     server_sign.initSign(privateKey);
     server_sign.update(contents); // For now – add security later
     return server_sign.sign();
   }
 
-  private boolean verifySignature(PublicKey publicKey, byte[] contents, byte[] signature) {
+  private boolean verifySignature(PublicKey publicKey, byte[] contents, byte[] signature) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
     Signature sign = Signature.getInstance("DSA");
     sign.initVerify(publicKey);
     sign.update(contents);
     return sign.verify(signature);
   }
 
-  private KeyPair createKeyPair() {
+  private KeyPair createKeyPair() throws NoSuchAlgorithmException {
     KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DSA");
     keyPairGenerator.initialize(2048);
     return keyPairGenerator.generateKeyPair();
   }
+
   public Boolean registerUser(String userId) throws RemoteException {
-    KeyPair keyPair = this.createKeyPair();
-    this.userKeys.put(userId, keyPair);
+    try {
+      KeyPair keyPair = this.createKeyPair();
+      this.userKeys.put(userId, keyPair);
 
-    byte[] signature = this.createSignature(keyPair.getPrivate(), userId.getBytes()); //TODO: Don't just sign userID
+      byte[] signature = this.createSignature(keyPair.getPrivate(), userId.getBytes()); //TODO: Don't just sign userID
 
-    AbstractAuthenticatedRegisterRequest req = new AuthenticatedRegisterRequest(userId, keyPair.getPublic().getEncoded(), signature);
+      AbstractAuthenticatedRegisterRequest req = new AuthenticatedRegisterRequest(userId, keyPair.getPublic().getEncoded(), signature);
 
-    AbstractAuthenticatedRegisterResponse response = this.network.handleAuthenticatedRegister(req);
+      AbstractAuthenticatedRegisterResponse response = this.network.handleAuthenticatedRegister(req);
 
-    boolean valid = this.verifySignature(this.createPublicKey(this.serverVerificationKey), response.status.name().getBytes(), response.digitalSignature); //TODO: Contents to verify will change
+      boolean valid = this.verifySignature(this.createPublicKey(this.serverVerificationKey), response.status.name().getBytes(), response.digitalSignature); //TODO: Contents to verify will change
 
-    return valid && response.status == AbstractAuthenticatedRegisterResponse.Status.OK;
+      return valid && response.status == AbstractAuthenticatedRegisterResponse.Status.OK;
+    } catch (Exception e) {
+      return false;
+    }
   }
 
   public Boolean create(String userId, K key, V val, M metaVal) throws RemoteException {
-    DoOperation<K, V, M> doOperation = new DoOperation<>(key, val, metaVal, DoOperation.Operation.CREATE);
-    byte[] signature = this.createSignature(userKeys.get(userId).getPrivate(), userId.getBytes()); //TODO: Don't just sign userId
+    try {
+      DoOperation<K, V, M> doOperation = new DoOperation<>(key, val, metaVal, DoOperation.Operation.CREATE);
+      byte[] signature = this.createSignature(userKeys.get(userId).getPrivate(), userId.getBytes()); //TODO: Don't just sign userId
 
-    AbstractAuthenticatedDoRequest<K,V,M> req = new AuthenticatedDoRequest<>(userId, doOperation, signature);
+      AbstractAuthenticatedDoRequest<K, V, M> req = new AuthenticatedDoRequest<>(userId, doOperation, signature);
 
-    AbstractAuthenticatedDoResponse<K,V,M> response = this.network.handleAuthenticatedDo(req);
+      AbstractAuthenticatedDoResponse<K, V, M> response = this.network.handleAuthenticatedDo(req);
 
-    boolean valid = this.verifySignature(this.createPublicKey(this.serverVerificationKey), response.outcome.outcome.name().getBytes(), response.digitalSignature);
+      boolean valid = this.verifySignature(this.createPublicKey(this.serverVerificationKey), response.outcome.outcome.name().getBytes(), response.digitalSignature);
 
-    return valid && response.outcome.outcome == DoOperationOutcome.Outcome.SUCCESS;
+      return valid && response.outcome.outcome == DoOperationOutcome.Outcome.SUCCESS;
+    } catch (Exception e) {
+      return false;
+    }
   }
 
   public Boolean delete(String userId, K key) throws RemoteException {
-    DoOperation<K, V, M> doOperation = new DoOperation<>(key, null, null, DoOperation.Operation.DELETE); // TODO: Make these null?
-    byte[] signature = this.createSignature(userKeys.get(userId).getPrivate(), userId.getBytes()); //TODO: Don't just sign userId
+    try {
+      DoOperation<K, V, M> doOperation = new DoOperation<>(key, null, null, DoOperation.Operation.DELETE); // TODO: Make these null?
+      byte[] signature = this.createSignature(userKeys.get(userId).getPrivate(), userId.getBytes()); //TODO: Don't just sign userId
 
-    AbstractAuthenticatedDoRequest<K,V,M> req = new AuthenticatedDoRequest<>(userId, doOperation, signature);
+      AbstractAuthenticatedDoRequest<K, V, M> req = new AuthenticatedDoRequest<>(userId, doOperation, signature);
 
-    AbstractAuthenticatedDoResponse<K,V,M> response = this.network.handleAuthenticatedDo(req);
+      AbstractAuthenticatedDoResponse<K, V, M> response = this.network.handleAuthenticatedDo(req);
 
-    boolean valid = this.verifySignature(this.createPublicKey(this.serverVerificationKey), response.outcome.outcome.name().getBytes(), response.digitalSignature);
+      boolean valid = this.verifySignature(this.createPublicKey(this.serverVerificationKey), response.outcome.outcome.name().getBytes(), response.digitalSignature);
 
-    return valid && response.outcome.outcome == DoOperationOutcome.Outcome.SUCCESS;
+      return valid && response.outcome.outcome == DoOperationOutcome.Outcome.SUCCESS;
+    } catch (Exception e) {
+      return false;
+    }
   }
 
   public V readVal(String userId, K key) throws RemoteException, NoSuchElementException {
-    DoOperation<K, V, M> doOperation = new DoOperation<>(key, null, null, DoOperation.Operation.READVAL); // TODO: Make these null?
-    byte[] signature = this.createSignature(userKeys.get(userId).getPrivate(), userId.getBytes()); //TODO: Don't just sign userId
+    try {
+      DoOperation<K, V, M> doOperation = new DoOperation<>(key, null, null, DoOperation.Operation.READVAL); // TODO: Make these null?
+      byte[] signature = this.createSignature(userKeys.get(userId).getPrivate(), userId.getBytes()); //TODO: Don't just sign userId
 
-    AbstractAuthenticatedDoRequest<K,V,M> req = new AuthenticatedDoRequest<>(userId, doOperation, signature);
+      AbstractAuthenticatedDoRequest<K, V, M> req = new AuthenticatedDoRequest<>(userId, doOperation, signature);
 
-    AbstractAuthenticatedDoResponse<K,V,M> response = this.network.handleAuthenticatedDo(req);
+      AbstractAuthenticatedDoResponse<K, V, M> response = this.network.handleAuthenticatedDo(req);
 
-    boolean valid = this.verifySignature(this.createPublicKey(this.serverVerificationKey), response.outcome.outcome.name().getBytes(), response.digitalSignature);
+      boolean valid = this.verifySignature(this.createPublicKey(this.serverVerificationKey), response.outcome.outcome.name().getBytes(), response.digitalSignature);
 
-    if (!valid || response.outcome.outcome == DoOperationOutcome.Outcome.AUTHENTICATION_FAILURE) { // TODO: Okay but like what
+      if (!valid || response.outcome.outcome == DoOperationOutcome.Outcome.AUTHENTICATION_FAILURE) { // TODO: Okay but like what
+        return null;
+      } else if (response.outcome.outcome == DoOperationOutcome.Outcome.SUCCESS) {
+        return response.outcome.val;
+      } else if (response.outcome.outcome == DoOperationOutcome.Outcome.NOSUCHELEMENT) {
+        throw new NoSuchElementException();
+      } else {
+        return null; // We should never get here!
+      }
+    } catch (Exception e) {
       return null;
-    } else if (response.outcome.outcome == DoOperationOutcome.Outcome.SUCCESS) {
-      return response.outcome.val;
-    } else if (response.outcome.outcome == DoOperationOutcome.Outcome.NOSUCHELEMENT) {
-      throw new NoSuchElementException();
-    } else {
-      return null; // We should never get here!
     }
   }
 
   public M readMetaVal(String userId, K key) throws RemoteException, NoSuchElementException {
-    DoOperation<K, V, M> doOperation = new DoOperation<>(key, null, null, DoOperation.Operation.READMETAVAL); // TODO: Make these null?
-    byte[] signature = this.createSignature(userKeys.get(userId).getPrivate(), userId.getBytes()); //TODO: Don't just sign userId
+    try {
+      DoOperation<K, V, M> doOperation = new DoOperation<>(key, null, null, DoOperation.Operation.READMETAVAL); // TODO: Make these null?
+      byte[] signature = this.createSignature(userKeys.get(userId).getPrivate(), userId.getBytes()); //TODO: Don't just sign userId
 
-    AbstractAuthenticatedDoRequest<K,V,M> req = new AuthenticatedDoRequest<>(userId, doOperation, signature);
+      AbstractAuthenticatedDoRequest<K, V, M> req = new AuthenticatedDoRequest<>(userId, doOperation, signature);
 
-    AbstractAuthenticatedDoResponse<K,V,M> response = this.network.handleAuthenticatedDo(req);
+      AbstractAuthenticatedDoResponse<K, V, M> response = this.network.handleAuthenticatedDo(req);
 
-    boolean valid = this.verifySignature(this.createPublicKey(this.serverVerificationKey), response.outcome.outcome.name().getBytes(), response.digitalSignature);
+      boolean valid = this.verifySignature(this.createPublicKey(this.serverVerificationKey), response.outcome.outcome.name().getBytes(), response.digitalSignature);
 
-    if (!valid || response.outcome.outcome == DoOperationOutcome.Outcome.AUTHENTICATION_FAILURE) { // TODO: Okay but like what
+      if (!valid || response.outcome.outcome == DoOperationOutcome.Outcome.AUTHENTICATION_FAILURE) { // TODO: Okay but like what
+        return null;
+      } else if (response.outcome.outcome == DoOperationOutcome.Outcome.SUCCESS) {
+        return response.outcome.metaVal;
+      } else if (response.outcome.outcome == DoOperationOutcome.Outcome.NOSUCHELEMENT) {
+        throw new NoSuchElementException();
+      } else {
+        return null; // We should never get here!
+      }
+    } catch (Exception e) {
       return null;
-    } else if (response.outcome.outcome == DoOperationOutcome.Outcome.SUCCESS) {
-      return response.outcome.metaVal;
-    } else if (response.outcome.outcome == DoOperationOutcome.Outcome.NOSUCHELEMENT) {
-      throw new NoSuchElementException();
-    } else {
-      return null; // We should never get here!
     }
   }
 
   public Boolean writeVal(String userId, K key, V newVal) throws RemoteException, IllegalArgumentException {
-    DoOperation<K, V, M> doOperation = new DoOperation<>(key, newVal, null, DoOperation.Operation.WRITEVAL); // TODO: Make these null?
-    byte[] signature = this.createSignature(userKeys.get(userId).getPrivate(), userId.getBytes()); //TODO: Don't just sign userId
+    try {
+      DoOperation<K, V, M> doOperation = new DoOperation<>(key, newVal, null, DoOperation.Operation.WRITEVAL); // TODO: Make these null?
+      byte[] signature = this.createSignature(userKeys.get(userId).getPrivate(), userId.getBytes()); //TODO: Don't just sign userId
 
-    AbstractAuthenticatedDoRequest<K,V,M> req = new AuthenticatedDoRequest<>(userId, doOperation, signature);
+      AbstractAuthenticatedDoRequest<K, V, M> req = new AuthenticatedDoRequest<>(userId, doOperation, signature);
 
-    AbstractAuthenticatedDoResponse<K,V,M> response = this.network.handleAuthenticatedDo(req);
+      AbstractAuthenticatedDoResponse<K, V, M> response = this.network.handleAuthenticatedDo(req);
 
-    boolean valid = this.verifySignature(this.createPublicKey(this.serverVerificationKey), response.outcome.outcome.name().getBytes(), response.digitalSignature);
+      boolean valid = this.verifySignature(this.createPublicKey(this.serverVerificationKey), response.outcome.outcome.name().getBytes(), response.digitalSignature);
 
-    if (!valid || response.outcome.outcome == DoOperationOutcome.Outcome.AUTHENTICATION_FAILURE) { // TODO: Okay but like what
+      if (!valid || response.outcome.outcome == DoOperationOutcome.Outcome.AUTHENTICATION_FAILURE) { // TODO: Okay but like what
+        return false;
+      } else if (response.outcome.outcome == DoOperationOutcome.Outcome.SUCCESS) {
+        return true;
+      } else if (response.outcome.outcome == DoOperationOutcome.Outcome.ILLEGALARGUMENT) {
+        throw new IllegalArgumentException();
+      } else {
+        return false; // We should never get here!
+      }
+    } catch (Exception e) {
       return false;
-    } else if (response.outcome.outcome == DoOperationOutcome.Outcome.SUCCESS) {
-      return true;
-    } else if (response.outcome.outcome == DoOperationOutcome.Outcome.ILLEGALARGUMENT) {
-      throw new IllegalArgumentException();
-    } else {
-      return false; // We should never get here!
     }
   }
 
   public Boolean writeMetaVal(String userId, K key, M newMetaVal) throws RemoteException, IllegalArgumentException {
-    DoOperation<K, V, M> doOperation = new DoOperation<>(key, null, newMetaVal, DoOperation.Operation.WRITEMETAVAL); // TODO: Make these null?
-    byte[] signature = this.createSignature(userKeys.get(userId).getPrivate(), userId.getBytes()); //TODO: Don't just sign userId
+    try {
+      DoOperation<K, V, M> doOperation = new DoOperation<>(key, null, newMetaVal, DoOperation.Operation.WRITEMETAVAL); // TODO: Make these null?
+      byte[] signature = this.createSignature(userKeys.get(userId).getPrivate(), userId.getBytes()); //TODO: Don't just sign userId
 
-    AbstractAuthenticatedDoRequest<K,V,M> req = new AuthenticatedDoRequest<>(userId, doOperation, signature);
+      AbstractAuthenticatedDoRequest<K, V, M> req = new AuthenticatedDoRequest<>(userId, doOperation, signature);
 
-    AbstractAuthenticatedDoResponse<K,V,M> response = this.network.handleAuthenticatedDo(req);
+      AbstractAuthenticatedDoResponse<K, V, M> response = this.network.handleAuthenticatedDo(req);
 
-    boolean valid = this.verifySignature(this.createPublicKey(this.serverVerificationKey), response.outcome.outcome.name().getBytes(), response.digitalSignature);
+      boolean valid = this.verifySignature(this.createPublicKey(this.serverVerificationKey), response.outcome.outcome.name().getBytes(), response.digitalSignature);
 
-    if (!valid || response.outcome.outcome == DoOperationOutcome.Outcome.AUTHENTICATION_FAILURE) { // TODO: Okay but like what
+      if (!valid || response.outcome.outcome == DoOperationOutcome.Outcome.AUTHENTICATION_FAILURE) { // TODO: Okay but like what
+        return false;
+      } else if (response.outcome.outcome == DoOperationOutcome.Outcome.SUCCESS) {
+        return true;
+      } else if (response.outcome.outcome == DoOperationOutcome.Outcome.ILLEGALARGUMENT) {
+        throw new IllegalArgumentException();
+      } else {
+        return false; // We should never get here!
+      }
+    } catch (Exception e) {
       return false;
-    } else if (response.outcome.outcome == DoOperationOutcome.Outcome.SUCCESS) {
-      return true;
-    } else if (response.outcome.outcome == DoOperationOutcome.Outcome.ILLEGALARGUMENT) {
-      throw new IllegalArgumentException();
-    } else {
-      return false; // We should never get here!
     }
   }
 }
